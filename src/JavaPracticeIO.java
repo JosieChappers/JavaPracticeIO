a4e6ebdf91b42bf5d870789d74c8f1a58950c61f
JosieChappers <160015530+JosieChappers@users.noreply.github.com>
2024-02-14 11:58:38 +0000

    Program code to write to screen and take data in from User

--- src/JavaPracticeIO.java
+++ src/JavaPracticeIO.java
@@ -1,9 +1,26 @@
+// use a library above the public class
+import java.util.Scanner; // This library is needed for input
+
 
 public class JavaPracticeIO {
 
 	public static void main(String[] args) {
-	
+		
+		//This is an output example	
 		System.out.println("Hello World!");
+		
+		//This is a variable used for output
+		String toPrint = "What is your favourite colour?";
+		
+		System.out.println(toPrint);
+		
+		//Input example
+		Scanner input = new Scanner(System.in);
+		//this variable is a Scanner type instead of a string or integer. It is an object
+		//the new means we are allocating memory to the new
+		String colourChoice = input.nextLine(); //input here is the variable name which is saved in colourChoice
+		System.out.println("Ah, I like "+  colourChoice+ " too");	
+		
 		
 	}
 	
