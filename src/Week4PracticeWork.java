c33a6ed91f414068d63befe1dd17ab238b0e2241
JosieChappers <160015530+JosieChappers@users.noreply.github.com>
2024-03-06 12:01:45 +0000

    Started the practice works

---
+++ src/Week4PracticeWork.java
@@ -0,0 +1,36 @@
+
+public class Week4PracticeWork {
+
+	public static void main(String[] args) {
+
+		// Starting strings
+		//Make 10 different string variables, called myString1 and so on. Put one to three words in each string. Try to write the strings so that when put together they make a sentence or a few sentences.
+		
+		String myString1 = "Hello.";
+		String myString2 = "It's Wednesday.";
+		String myString3 = "What's";
+		String myString4 = "occuring?!";
+		String myString5 = "I can't wait";
+		String myString6 = "to eat my lunch.";
+		String myString7 = "But I";
+		String myString8 = "need";
+		//System.out.println(myString8.toUpperCase());		
+		
+		String myString9 = "to";
+		String myString10 = "RUN!!";
+		
+		//Create another string variable called combinedString. Add all the other strings together. 
+		String combinedString = myString1 + " " + myString2 + " " + myString3 + " " + myString4 + " " + myString5 + " " + myString6 + " " + myString7 + " " + myString8 + " " + myString9 + " " + myString10; 
+	
+		//Print out the combinedString.
+		System.out.println(combinedString);
+		
+		// toUpperCase
+		//Change three of the variables (your choice) to all upper case. 
+		//Do this BEFORE adding them all together and printing them out.
+
+
+		
+	}
+
+}
